3763bf89b022f35a45259d8c1b23290933798fc1
Maksim Kulikov <kulikovman@gmail.com>
2017-03-27 14:12:07 +0500

    Changed the initialization of buttons

--- app/src/main/java/ru/kulikovman/calcus/MainActivity.java
+++ app/src/main/java/ru/kulikovman/calcus/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     private Button btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9, btn_0,
             btn_addition, btn_subtraction, btn_multiplication, btn_devision,
             btn_dot, btn_reset, btn_delete, btn_calculate;
@@ -25,12 +25,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initAllItems();
     }
 
-    @Override
-    public void onClick(View v) {
-        v.setHapticFeedbackEnabled(true);
-        v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+    public void onClick(View view) {
+        view.setHapticFeedbackEnabled(true);
+        view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
 
-        switch (v.getId()) {
+        switch (view.getId()) {
             case R.id.btn_1:
                 temp += 1;
                 textResult.setText(String.valueOf(temp));
@@ -43,7 +42,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initAllItems() {
-        //initializing all items
         btn_1 = (Button) findViewById(R.id.btn_1);
         btn_2 = (Button) findViewById(R.id.btn_2);
         btn_3 = (Button) findViewById(R.id.btn_3);
@@ -65,8 +63,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textResult = (TextView) findViewById(R.id.textResult);
         textCalculation = (TextView) findViewById(R.id.textCalculation);
 
-        //пока не инициализирую, все равно не могу использовать...
-        /*btn_000 = (Button) findViewById(R.id.btn_000);
+        btn_000 = (Button) findViewById(R.id.btn_000);
         btn_sinus = (Button) findViewById(R.id.btn_sinus);
         btn_cosine = (Button) findViewById(R.id.btn_cosine);
         btn_tangent = (Button) findViewById(R.id.btn_tangent);
@@ -78,43 +75,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_power = (Button) findViewById(R.id.btn_power);
         btn_opening_bracket = (Button) findViewById(R.id.btn_opening_bracket);
         btn_closing_bracket = (Button) findViewById(R.id.btn_closing_bracket);
-        btn_square_root = (Button) findViewById(R.id.btn_square_root);*/
-
-
-        //assign button listener
-        btn_1.setOnClickListener(this);
-        btn_2.setOnClickListener(this);
-        btn_3.setOnClickListener(this);
-        btn_4.setOnClickListener(this);
-        btn_5.setOnClickListener(this);
-        btn_6.setOnClickListener(this);
-        btn_7.setOnClickListener(this);
-        btn_8.setOnClickListener(this);
-        btn_9.setOnClickListener(this);
-        btn_0.setOnClickListener(this);
-        btn_addition.setOnClickListener(this);
-        btn_subtraction.setOnClickListener(this);
-        btn_multiplication.setOnClickListener(this);
-        btn_devision.setOnClickListener(this);
-        btn_dot.setOnClickListener(this);
-        btn_reset.setOnClickListener(this);
-        btn_delete.setOnClickListener(this);
-        btn_calculate.setOnClickListener(this);
-        textResult.setOnClickListener(this);
-
-        //кнопки альбомного режима не получается активировать...
-        /*btn_000.setOnClickListener(this);
-        btn_sinus.setOnClickListener(this);
-        btn_cosine.setOnClickListener(this);
-        btn_tangent.setOnClickListener(this);
-        btn_natural_logarithm.setOnClickListener(this);
-        btn_logarithm.setOnClickListener(this);
-        btn_percent.setOnClickListener(this);
-        btn_pi.setOnClickListener(this);
-        btn_e.setOnClickListener(this);
-        btn_power.setOnClickListener(this);
-        btn_opening_bracket.setOnClickListener(this);
-        btn_closing_bracket.setOnClickListener(this);
-        btn_square_root.setOnClickListener(this);*/
+        btn_square_root = (Button) findViewById(R.id.btn_square_root);
     }
 }
