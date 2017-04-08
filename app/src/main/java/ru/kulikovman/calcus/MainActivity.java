fbe1600aedd80462f259fd677e7983c7b64d09f6
Maksim Kulikov <kulikovman@gmail.com>
2017-04-08 10:54:33 +0500

    Removed extra code

--- app/src/main/java/ru/kulikovman/calcus/MainActivity.java
+++ app/src/main/java/ru/kulikovman/calcus/MainActivity.java
@@ -5,14 +5,10 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import android.os.Build;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.HapticFeedbackConstants;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,12 +24,6 @@ public class MainActivity extends AppCompatActivity {
         resultField = (TextView) findViewById(R.id.resultField);
         calculationField = (TextView) findViewById(R.id.calculationField);
         memoryField = (TextView) findViewById(R.id.memoryField);
-
-        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
-        }*/
     }
 
 
